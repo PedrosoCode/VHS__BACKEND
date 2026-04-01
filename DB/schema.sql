f3b512ee060d99193ec6db590a3d14a39adb7ca9
gabriel <gb.ribeiropedroso@gmail.com>
2026-03-31 21:42:56 -0300

    Procedure insert mensagem + ajuste PK

--- DB/schema.sql
+++ DB/schema.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict m9ZR36RFiFzffOHgylSirVbQtcJoKsu6dgfAZwQQzkW3neZ0SCYidbsUGg7c4fM
+\restrict I9aiuXWG40j7BFMOd3SwJzJmcQTWCv0rYereuAmB9TGMCw65LJNawcem3o2I4iW
 
 -- Dumped from database version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.11 (Ubuntu 16.11-0ubuntu0.24.04.1)
 
--- Started on 2026-03-28 20:38:36 -03
+-- Started on 2026-03-31 21:42:07 -03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,7 +21,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 232 (class 1255 OID 17964)
+-- TOC entry 233 (class 1255 OID 17964)
 -- Name: fn_select_chat_grupo_mensagem(bigint); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
@@ -54,6 +54,46 @@ $$;
 
 
 ALTER FUNCTION public.fn_select_chat_grupo_mensagem(p_codigo_empresa_chat bigint) OWNER TO postgres;
+
+--
+-- TOC entry 225 (class 1255 OID 17985)
+-- Name: sp_insert_chat_grupo_mensagem(integer, integer, integer, integer, text); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.sp_insert_chat_grupo_mensagem(IN p_codigo_grupo integer, IN p_codigo_empresa_grupo integer, IN p_codigo_usuario_remetente integer, IN p_codigo_empresa_usuario_remetente integer, IN p_mensagem text)
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    v_codigo_mensagem INT;
+BEGIN
+    SELECT COALESCE(MAX(codigo_mensagem), 0) + 1
+      INTO v_codigo_mensagem
+      FROM tb_chat_grupo_mensagem
+     WHERE codigo_empresa_grupo = p_codigo_empresa_grupo
+       AND codigo_grupo = p_codigo_grupo;
+
+    INSERT INTO tb_chat_grupo_mensagem (
+        codigo_mensagem,
+        codigo_grupo,
+        codigo_empresa_grupo,
+        codigo_usuario,
+        codigo_empresa_usuario,
+        mensagem,
+        data_input
+    ) VALUES (
+        v_codigo_mensagem,
+        p_codigo_grupo,
+        p_codigo_empresa_grupo,
+        p_codigo_usuario_remetente,
+        p_codigo_empresa_usuario_remetente,
+        p_mensagem,
+        NOW()
+    );
+END;
+$$;
+
+
+ALTER PROCEDURE public.sp_insert_chat_grupo_mensagem(IN p_codigo_grupo integer, IN p_codigo_empresa_grupo integer, IN p_codigo_usuario_remetente integer, IN p_codigo_empresa_usuario_remetente integer, IN p_mensagem text) OWNER TO postgres;
 
 SET default_tablespace = '';
 
@@ -138,7 +178,7 @@ ALTER TABLE public.tb_chat_grupo OWNER TO postgres;
 CREATE TABLE public.tb_chat_grupo_mensagem (
     codigo_mensagem bigint NOT NULL,
     codigo_grupo bigint NOT NULL,
-    codigo_empresa_grupo bigint,
+    codigo_empresa_grupo bigint NOT NULL,
     codigo_usuario bigint,
     codigo_empresa_usuario bigint,
     mensagem text,
@@ -149,7 +189,7 @@ CREATE TABLE public.tb_chat_grupo_mensagem (
 ALTER TABLE public.tb_chat_grupo_mensagem OWNER TO postgres;
 
 --
--- TOC entry 3431 (class 0 OID 17911)
+-- TOC entry 3432 (class 0 OID 17911)
 -- Dependencies: 215
 -- Data for Name: tb_cad_empresa; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -162,7 +202,7 @@ COPY public.tb_cad_empresa (codigo, nome_fantasia, razao_social, presta_suporte)
 
 
 --
--- TOC entry 3436 (class 0 OID 17980)
+-- TOC entry 3437 (class 0 OID 17980)
 -- Dependencies: 220
 -- Data for Name: tb_cad_grupo_empresa; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -174,7 +214,7 @@ COPY public.tb_cad_grupo_empresa (codigo, descricao) FROM stdin;
 
 
 --
--- TOC entry 3435 (class 0 OID 17970)
+-- TOC entry 3436 (class 0 OID 17970)
 -- Dependencies: 219
 -- Data for Name: tb_cad_grupo_empresa_empresas; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -186,7 +226,7 @@ COPY public.tb_cad_grupo_empresa_empresas (codigo_grupo, codigo_empresa) FROM st
 
 
 --
--- TOC entry 3432 (class 0 OID 17918)
+-- TOC entry 3433 (class 0 OID 17918)
 -- Dependencies: 216
 -- Data for Name: tb_cad_usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -202,7 +242,7 @@ COPY public.tb_cad_usuario (codigo, codigo_empresa, usuario, nome, usuario_adm) 
 
 
 --
--- TOC entry 3433 (class 0 OID 17930)
+-- TOC entry 3434 (class 0 OID 17930)
 -- Dependencies: 217
 -- Data for Name: tb_chat_grupo; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -216,7 +256,7 @@ COPY public.tb_chat_grupo (codigo, codigo_empresa, titulo) FROM stdin;
 
 
 --
--- TOC entry 3434 (class 0 OID 17940)
+-- TOC entry 3435 (class 0 OID 17940)
 -- Dependencies: 218
 -- Data for Name: tb_chat_grupo_mensagem; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -226,11 +266,12 @@ COPY public.tb_chat_grupo_mensagem (codigo_mensagem, codigo_grupo, codigo_empres
 2	1	3	1	1	como podemos ajudar?	2026-03-28 19:49:51.618115
 3	1	3	1	3	Os apontamentos estão travados	2026-03-28 19:52:37.779362
 4	1	3	2	3	Acho que o APP caiu	2026-03-28 20:24:03.230255
+5	1	3	1	1	olá mundo	2026-03-31 21:35:45.047146
 \.
 
 
 --
--- TOC entry 3276 (class 2606 OID 17934)
+-- TOC entry 3277 (class 2606 OID 17934)
 -- Name: tb_chat_grupo pk_chat_grupo; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -239,7 +280,7 @@ ALTER TABLE ONLY public.tb_chat_grupo
 
 
 --
--- TOC entry 3272 (class 2606 OID 17917)
+-- TOC entry 3273 (class 2606 OID 17917)
 -- Name: tb_cad_empresa pk_empresa; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -248,7 +289,7 @@ ALTER TABLE ONLY public.tb_cad_empresa
 
 
 --
--- TOC entry 3282 (class 2606 OID 17984)
+-- TOC entry 3283 (class 2606 OID 17984)
 -- Name: tb_cad_grupo_empresa pk_grupo_empresa; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -257,7 +298,7 @@ ALTER TABLE ONLY public.tb_cad_grupo_empresa
 
 
 --
--- TOC entry 3280 (class 2606 OID 17974)
+-- TOC entry 3281 (class 2606 OID 17974)
 -- Name: tb_cad_grupo_empresa_empresas pk_grupo_empresa_empresas; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -266,16 +307,16 @@ ALTER TABLE ONLY public.tb_cad_grupo_empresa_empresas
 
 
 --
--- TOC entry 3278 (class 2606 OID 17946)
+-- TOC entry 3279 (class 2606 OID 17987)
 -- Name: tb_chat_grupo_mensagem pk_grupo_mensagem; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.tb_chat_grupo_mensagem
-    ADD CONSTRAINT pk_grupo_mensagem PRIMARY KEY (codigo_mensagem, codigo_grupo);
+    ADD CONSTRAINT pk_grupo_mensagem PRIMARY KEY (codigo_mensagem, codigo_grupo, codigo_empresa_grupo);
 
 
 --
--- TOC entry 3274 (class 2606 OID 17924)
+-- TOC entry 3275 (class 2606 OID 17924)
 -- Name: tb_cad_usuario pk_usuario; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -284,7 +325,7 @@ ALTER TABLE ONLY public.tb_cad_usuario
 
 
 --
--- TOC entry 3285 (class 2606 OID 17947)
+-- TOC entry 3286 (class 2606 OID 17988)
 -- Name: tb_chat_grupo_mensagem fk_empresa_grupo_mensagem; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -293,7 +334,7 @@ ALTER TABLE ONLY public.tb_chat_grupo_mensagem
 
 
 --
--- TOC entry 3286 (class 2606 OID 17952)
+-- TOC entry 3287 (class 2606 OID 17952)
 -- Name: tb_chat_grupo_mensagem fk_empresa_grupo_mensagem_usuario; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -302,7 +343,7 @@ ALTER TABLE ONLY public.tb_chat_grupo_mensagem
 
 
 --
--- TOC entry 3284 (class 2606 OID 17935)
+-- TOC entry 3285 (class 2606 OID 17935)
 -- Name: tb_chat_grupo fk_grupo_empresa; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -311,7 +352,7 @@ ALTER TABLE ONLY public.tb_chat_grupo
 
 
 --
--- TOC entry 3287 (class 2606 OID 17975)
+-- TOC entry 3288 (class 2606 OID 17975)
 -- Name: tb_cad_grupo_empresa_empresas fk_grupo_empresa_empresas; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -320,7 +361,7 @@ ALTER TABLE ONLY public.tb_cad_grupo_empresa_empresas
 
 
 --
--- TOC entry 3283 (class 2606 OID 17925)
+-- TOC entry 3284 (class 2606 OID 17925)
 -- Name: tb_cad_usuario fk_usuario_empresa; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -328,11 +369,11 @@ ALTER TABLE ONLY public.tb_cad_usuario
     ADD CONSTRAINT fk_usuario_empresa FOREIGN KEY (codigo_empresa) REFERENCES public.tb_cad_empresa(codigo);
 
 
--- Completed on 2026-03-28 20:38:36 -03
+-- Completed on 2026-03-31 21:42:07 -03
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict m9ZR36RFiFzffOHgylSirVbQtcJoKsu6dgfAZwQQzkW3neZ0SCYidbsUGg7c4fM
+\unrestrict I9aiuXWG40j7BFMOd3SwJzJmcQTWCv0rYereuAmB9TGMCw65LJNawcem3o2I4iW
 
